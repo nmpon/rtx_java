e4ee5fd8dc13a3688bc12362d5ec5ed97a7c46ca
nik <n.m.ponomarev@gmail.com>
2026-09-24 19:41:55 +0200

    introduce fov

--- src/Camera.java
+++ src/Camera.java
@@ -1,9 +1,11 @@
 public class Camera {
     Vector3 position;
     Vector3 rotation;
+    double fieldOfView;
 
-    Camera(Vector3 position, Vector3 rotation) {
+    Camera(Vector3 position, Vector3 rotation, double fieldOfView) {
         this.position = position;
         this.rotation = rotation;
+        this.fieldOfView = fieldOfView;
     }
 }
