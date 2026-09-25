d3bdb295de504ae01651f9a529d32cb9cf8337fe
Nikita <118979770+mininuke69@users.noreply.github.com>
2026-09-25 18:36:06 +0200

    color mixing

--- src/Color.java
+++ src/Color.java
@@ -13,6 +13,17 @@ public class Color {
         return new Color(this.red * other.red, this.green * other.green, this.blue * other.blue);
     }
 
+    public Color mix(Color other, double weight) {
+        // mixed using x = (1-w) * this + (w-1) * other
+        // 0 <= x <= 1
+        // higher x -> more other color
+        return new Color(
+                this.red * (1 - weight) + other.red * (weight - 1),
+                this.green * (1 - weight) + other.green * (weight - 1),
+                this.blue * (1 - weight) + other.blue * (weight - 1)
+        );
+    }
+
     public Color add(Color other) {
         return new Color(this.red + other.red, this.green + other.green, this.blue + other.blue);
     }
