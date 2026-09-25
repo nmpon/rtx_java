c087b51df4100325cd1de286636e9f9cdd6721d9
Nikita <118979770+mininuke69@users.noreply.github.com>
2026-09-25 22:52:47 +0200

    emission and diffuse shaders

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,17 @@ AA pixel mixing
 void main() throws IOException {
     var sceneObjects = new ArrayList<Sphere>();
 
-    sceneObjects.add(new Sphere(2.0, new Vector3(0.0, 0.0, 10), new Shader(new Color(.7, .8, .1))));
-    sceneObjects.add(new Sphere(1.5, new Vector3(2.0, -5.0, 12), new Shader(new Color(.8, .6, .8))));
-    sceneObjects.add(new Sphere(2.5, new Vector3(-5.0, 2.0, 8), new Shader(new Color(.2, .3, .4))));
-    sceneObjects.add(new Sphere(2.0, new Vector3(4.0, -2.0, 8), new Shader(new Color(2, 2, 2))));
+    sceneObjects.add(new Sphere(2.0, new Vector3(0.0, 0.0, 10), Shader.shaderDiffuse(new Color(.7, .7, .1))));
+    sceneObjects.add(new Sphere(1.5, new Vector3(2.0, -5.0, 12), Shader.shaderDiffuse(new Color(.8, .6, .8))));
+    sceneObjects.add(new Sphere(2.5, new Vector3(-5.0, 2.0, 8), Shader.shaderDiffuse(new Color(.2, .3, .4))));
+    sceneObjects.add(new Sphere(2.0, new Vector3(4.0, -2.0, 8), Shader.shaderEmission(new Color(9, 9, 9))));
     var camera = new Camera(
             new Vector3(0, 0, 0),
             new Vector3(0, 0, 0),
             Math.toRadians(50)
     );
-    var scene = new Scene(sceneObjects, camera);
-    var outputImage = RenderEngine.renderSimple(scene, 1280, 720, 1000, 12);
+    var scene = new Scene(sceneObjects, camera, new Color(0.2, 0.2, 0.2));
+    var outputImage = RenderEngine.renderSimple(scene, 1280, 720, 50, 12);
 
     File outputfile = new File("output.png");
     ImageIO.write(outputImage, "png", outputfile);
--- src/Shader.java
+++ src/Shader.java
@@ -1,17 +1,49 @@
 public class Shader {
-    Color color;
 
-    Shader(Color color) {
+    enum ShaderType {
+        DIFFUSE,
+        EMISSION
+    }
+
+    Color color;
+    ShaderType shaderType;
+
+    private Shader(ShaderType shaderType, Color color) {
+        this.shaderType = shaderType;
         this.color = color;
+    }
+
+    public static Shader shaderDiffuse(Color color) {
+        return new Shader(ShaderType.DIFFUSE, color);
+    }
+
+    public static Shader shaderEmission(Color color) {
+        return new Shader(ShaderType.EMISSION, color);
     }
 
     Color calculateColor(Vector3 incoming, Vector3 outgoing, Vector3 normal, Color incomingColor) {
         // use simple diffuse + color.mix for now
-        var weight = simpleDiffuse(outgoing, normal);
-        return this.color.mix(incomingColor, weight);
+        double weight;
+        switch (this.shaderType) {
+            case DIFFUSE -> {
+                weight = simpleDiffuse(incoming, normal);
+            }
+            case EMISSION -> {
+                weight = simpleEmission();
+            }
+            default -> {
+                throw new IllegalArgumentException("Invalid shader type");
+            }
+        }
+        return incomingColor.mix(this.color, weight);
+
     }
 
     double simpleDiffuse(Vector3 outgoing, Vector3 normal) {
         return Math.max(0, outgoing.normalized().dotProduct(normal.normalized()));
+    }
+
+    double simpleEmission() {
+        return 1;
     }
 }
